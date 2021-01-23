2a34ecebc90692274c74180801c4ef6dc2012bb0
İlyas Akın <ashnwor@gmail.com>
2021-01-23 18:09:04 +0300

    Setup cosmiconfig & jest

--- src/deliquus.js
+++ src/deliquus.js
@@ -1,1 +1,24 @@
+import { cosmiconfig } from 'cosmiconfig';
+
+const explorer = cosmiconfig('deliquus');
+
+explorer
+  .search()
+  .then((result) => {
+    // result.config is the parsed configuration object.
+    // result.filepath is the path to the config file that was found.
+    // result.isEmpty is true if there was nothing to parse in the config file.
+
+    if (result.isEmpty) {
+      console.log('Nothing to parse in the config');
+      process.exit(1);
+    }
+
+    console.log(result.config);
+  })
+  .catch(() => {
+    console.log('No config found');
+    process.exit(1);
+  });
+
 console.log('Deliquus Project');
