1954dea29b1a233c2e44aaeca935f8bcb75a3faf
İlyas Akın <ashnwor@gmail.com>
2021-01-26 22:44:12 +0300

    Add some early returns

--- src/deliquus.js
+++ src/deliquus.js
@@ -9,18 +9,38 @@ const main = async () => {
     const explorer = await cosmiconfig('deliquus').search();
 
     if (explorer.isEmpty) {
-      console.log(c.red('Nothing to parse in the config'));
+      console.error(c.red('Nothing to parse in the config'));
       process.exit(1);
     }
 
     ctx.explorer = explorer;
   } catch (error) {
-    console.log(c.red('No config found'));
+    console.error(c.red('No config found'));
     process.exit(1);
   }
 
   const { paths, extensions, testExtensions, storyExtensions } = ctx.explorer.config;
   console.log({ paths, extensions, testExtensions, storyExtensions });
+
+  if (!paths || paths.length === 0) {
+    console.error(c.red('No path found'));
+    process.exit(1);
+  }
+
+  if (!extensions || extensions.length === 0) {
+    console.error(c.red('No extension found'));
+    process.exit(1);
+  }
+
+  if (!testExtensions || testExtensions.length === 0) {
+    console.error(c.red('No test extension found'));
+    process.exit(1);
+  }
+
+  if (!storyExtensions || storyExtensions.length === 0) {
+    console.error(c.red('No story extension found'));
+    process.exit(1);
+  }
 };
 
 main();
