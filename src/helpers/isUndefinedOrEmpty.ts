9d38a3769a4bec6a8dfe05fd28bc37bd835957fb
İlyas Akın <ashnwor@gmail.com>
2021-02-04 23:45:46 +0300

    Add isUndefinedOrEmpty test

--- src/helpers/isUndefinedOrEmpty.ts
+++ src/helpers/isUndefinedOrEmpty.ts
@@ -1,4 +1,4 @@
-const isUndefinedOrEmpty = (array: string[]): boolean => {
+const isUndefinedOrEmpty = (array: string[] | undefined): boolean => {
   if (!array || array.length === 0) return true;
   return false;
 };
