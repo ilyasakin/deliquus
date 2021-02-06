74a12a2c65f54755311027cc92ac53bcf79a40e3
İlyas Akın <ashnwor@gmail.com>
2021-02-06 21:55:08 +0300

    Implement initial source & target comparison

--- src/deliquus.js
+++ src/deliquus.js
@@ -5,6 +5,8 @@ import Context from './helpers/context.ts';
 import crash from './helpers/crash.ts';
 import isUndefinedOrEmpty from './helpers/isUndefinedOrEmpty.ts';
 import filterByExtension from './helpers/filterByExtension';
+import removeExtensions from './helpers/removeExtensions';
+import areArraysEqual from './helpers/areArraysEqual';
 
 const main = async () => {
   let context = null;
@@ -30,10 +32,21 @@ const main = async () => {
   if (isUndefinedOrEmpty(targets)) crash('No target found');
   if (isUndefinedOrEmpty(extensions)) crash('No extension found');
 
+  const sourcesFilenames = fs.readdirSync(`${process.cwd()}/${sources[0]}`);
   const targetsFilenames = fs.readdirSync(`${process.cwd()}/${targets[0]}`);
 
-  console.log(targetsFilenames);
-  console.log(filterByExtension(targetsFilenames, extensions[0]));
+  const filteredSources = filterByExtension(sourcesFilenames, 'ts');
+  console.log(filteredSources);
+  const filteredTargets = filterByExtension(targetsFilenames, extensions[0]);
+  console.log(filteredTargets);
+
+  const sourcesFilenamesWoExtensions = removeExtensions(filteredSources, 'ts');
+  console.log(sourcesFilenamesWoExtensions);
+  const targetsFilenamesWoExtensions = removeExtensions(filteredTargets, 'test.ts');
+  console.log(targetsFilenamesWoExtensions);
+
+  if (!areArraysEqual(sourcesFilenamesWoExtensions, targetsFilenamesWoExtensions))
+    crash('Not same');
 };
 
 main();
