73fb96d4a013567a0ab4e35669eec3b3e1d22b54
İlyas Akın <ashnwor@gmail.com>
2021-02-12 20:09:47 +0300

    Remove unused functions

--- src/helpers/removeExtensions.ts
+++
@@ -1,6 +0,0 @@
-const removeExtensions = (target: string[]): string[] => {
-  const regex = /[^.]*/;
-  return target.map((value) => (regex.exec(value) as string[])[0]);
-};
-
-export default removeExtensions;
