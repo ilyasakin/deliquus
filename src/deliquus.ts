cc8fca666d41d04d8d1b44ae9381a78760b44bfa
İlyas Akın <ashnwor@gmail.com>
2021-02-16 20:27:15 +0300

    Bump version to 0.3.5

--- src/deliquus.ts
+++ src/deliquus.ts
@@ -14,7 +14,7 @@ import validateConfig from './helpers/validateConfig';
 import parsePath from './helpers/parsePath';
 import Explorer from './types/Explorer';
 
-program.version('0.3.0').option('-c, --continueWhenFailed', 'Returns with 0 if fails');
+program.version('0.3.5').option('-c, --continueWhenFailed', 'Returns with 0 if fails');
 program.parse(process.argv);
 
 const main = async () => {
