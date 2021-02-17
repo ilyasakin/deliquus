4c2fb0171605f3a6b3bd5541a3252b3e581bd57b
İlyas Akın <ashnwor@gmail.com>
2021-02-17 13:04:05 +0300

    Remove DEBUG env flag on yarn test script
    and move it to related test file

--- src/__tests__/debug.test.ts
+++ src/__tests__/debug.test.ts
@@ -2,6 +2,7 @@ import debug from '../helpers/debug';
 
 describe('debug', () => {
   it('executes console.log', () => {
+    process.env = { DEBUG: '1' };
     const mockConsole = jest.spyOn(console, 'log').mockImplementation();
     debug('test');
     expect(mockConsole).toHaveBeenCalled();
